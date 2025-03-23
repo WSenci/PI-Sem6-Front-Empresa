827bb3fe5fa9292689da5af5a7e3ff59a041baa4
Caio Assis <caio.ab.assis@gmail.com>
2025-03-23 15:08:44 -0300

    Altera aparencia dos cards

--- app/kitchen/pedidos.tsx
+++ app/kitchen/pedidos.tsx
@@ -1,8 +1,10 @@
 import { router } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, ScrollView, Modal, TextInput, Button, TouchableWithoutFeedback } from 'react-native';
+import { SButtonItemCozinha } from '../../components/styled-button';
 
 import { mockupOrder } from '../../components/mockups/orders-mockup';
+import { IProdutoCom } from '../../components/interfaces';
 
 /*
 Pensar em como o sistema vai atualizar os pedidos feitos pelo cardapio, quando forem feitos
@@ -13,6 +15,8 @@ Trocar cor do item já realizado pela cozinha?
 
 export default function KitchenOrdersScreen() {
   const [orders, setOrders] = useState(mockupOrder);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [orderItem, setOrderItem] = useState<IProdutoCom>({ nome: '', preco: 0, tipo: '', comment: '', desc: '' })
 
   function getHourFromDate(date: Date): string {
     return date.toISOString().split('T')[1].split('.')[0]
@@ -20,21 +24,57 @@ export default function KitchenOrdersScreen() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.homeButton} onPress={() => router.navigate("/")}> 
+
+      <TouchableOpacity style={styles.homeButton} onPress={() => router.navigate("/")}>
         <Text style={styles.homeButtonText}>Voltar</Text>
       </TouchableOpacity>
+
       <Text style={styles.title}>Pedidos na Cozinha</Text>
-      
+
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {orders.map((order) => (
+
           <View key={order._id} style={styles.orderCard}>
+
             <Text style={styles.orderNumber}>{getHourFromDate(order.data_pedido)}</Text>
-            {order.produtos.map((item, index) => (
-              <Text key={index} style={styles.itemText}>{item.nome}</Text>
+            {order.produtos.map((item) => (
+
+              <View>
+                <SButtonItemCozinha item={item} onClick={() => {
+                  setModalVisible(true)
+                  setOrderItem(item)
+                }} />
+              </View>
+
             ))}
           </View>
+
         ))}
       </ScrollView>
+
+
+      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+          <View style={styles.modalContainer}>
+            <TouchableWithoutFeedback onPress={() => { }} >
+              <View style={styles.modalContent}>
+
+                <Text style={styles.modalTitle}>{orderItem.nome}</Text>
+                <Text style={styles.modalDescription}>{orderItem.desc}</Text>
+                <Text style={[styles.modalDescription, {fontWeight:'bold'}]}>{orderItem.comment}</Text>
+
+                <View style={{ flex: 1, flexDirection:'row', justifyContent:'space-evenly', width: '60%'}}>
+                  
+                  <TouchableOpacity onPress={() => setModalVisible(false)}>
+                    <Text style={{ color: 'red', fontSize: 20, backgroundColor: '#F5F5F5', borderRadius: 8 }}> X </Text>
+                  </TouchableOpacity>
+
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </View>
   );
 }
@@ -45,6 +85,28 @@ const styles = StyleSheet.create({
   homeButtonText: { color: 'white', fontWeight: 'bold' },
   title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' },
   orderCard: { backgroundColor: 'white', padding: 15, marginHorizontal: 10, borderRadius: 8, elevation: 3, minWidth: 200 },
-  orderNumber: { fontSize: 18, fontWeight: 'bold', marginBottom: 10 },
+  orderNumber: { fontSize: 20, fontWeight: 'bold', marginBottom: 10, alignSelf:'center'},
   itemText: { fontSize: 16 },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: 300,
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  modalDescription: {
+    fontSize: 14,
+    marginVertical: 10,
+    textAlign: "center",
+  },
 });
--- components/styled-button/index.tsx
+++ components/styled-button/index.tsx
@@ -1,24 +1,31 @@
-import { Button } from "react-native-paper";
-import { Dimensions, StyleSheet } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { Dimensions, StyleSheet, View } from "react-native";
 import { IProdutoCom } from "../interfaces";
 
-export function SButtonItemCozinha(Item:IProdutoCom){
-    function handleButton(){
-        console.log('yes')
-    }
+interface ISButtonItemCozinha{
+    item:IProdutoCom
+    onClick: () => void
+}
+
+export function SButtonItemCozinha({ item, onClick }: ISButtonItemCozinha){
     return(
-        <Button mode="contained" style={styles.itemCozinha} onPress={handleButton}>Login</Button>
+        <Button mode="contained" labelStyle={{color:'black', fontSize:16}} style={[styles.itemCozinha, {backgroundColor:item.comment? '#dee09b': 'white'}]} onPress={onClick}>
+            <View style={{alignItems:'center'}}>
+            <Text style={{fontWeight:'bold'}}>{item.nome}</Text>
+            <Text style={{marginTop:3}}>{item.comment}</Text>
+            </View>
+        </Button>
     )
 }
 
 const styles = StyleSheet.create({
     itemCozinha: {
-        fontSize: 16,
         marginBottom: 10,
-        backgroundColor: 'gray',
         padding: 5,
         marginHorizontal: 10,
         borderRadius: 8,
-        minWidth: 150
+        minWidth: 150,
+        borderWidth:1,
+        borderColor:'black'
     }
 })
