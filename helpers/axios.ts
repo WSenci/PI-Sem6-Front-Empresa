2e2439b3b22ab5a89c1ef5008eb4bad9c14ec8a7
Caio Assis <caio.ab.assis@gmail.com>
2025-04-13 19:40:51 -0300

    integra cozinha com back end

---
+++ helpers/axios.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import Constants from "expo-constants";
+
+//const uri = Constants.expoConfig?.hostUri ? `http://${Constants.expoConfig?.hostUri?.split(':').shift()}:3000` :  "";
+const uri = 'http://localhost:3000'
+
+const api = axios.create({
+    baseURL: uri,
+    timeout: 2000,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
+
+export default api;
